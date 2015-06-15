2d1b3761601372efa7e2570b2a7fe25677acdd56
Guest User <student@epicodus.com>
2015-06-15 11:49:11 -0700

    will show individual post in posts

---
+++ server/mocks/posts.js
@@ -0,0 +1,73 @@
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+var posts = [
+  {id: 1, title: 'I love blood!', author: 1, body: "Blood is delicious."},
+  {id: 2, title: 'Give me your blood', author: 1, body: "I need some blood."},
+  {id: 3, title: 'Seriously, I am so hungry!', author: 2, body: "I am hungry."}
+]
+
+var authors = [
+  {id:1, name: 'Dracula', posts: [1,2]},
+  {id:2, name: 'Bob', posts: [3]}
+]
+
+
+module.exports = function(app) {
+  var express = require('express');
+  var postsRouter = express.Router();
+
+  postsRouter.get('/', function(req, res) {
+    res.send({
+      'posts': posts,
+      'authors': authors
+    });
+
+  });
+
+  postsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  postsRouter.get('/:id', function(req, res) {
+    res.send({
+      'post': posts.find(function(post) {
+        return post.id == req.params.id;
+      }),
+      'authors': authors
+    });
+  });
+
+  postsRouter.put('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/posts', postsRouter);
+};
